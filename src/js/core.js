ade297f74c423f6bf91b1148464c0cc5bd5b8ce7
Remmy <goremmy@gmail.com>
2017-10-03 22:36:02 -0400

    refactor `getResource`

--- src/js/core.js
+++ src/js/core.js
@@ -9,9 +9,26 @@ var cfg = allCfg[nodeEnv];
 
 module.exports.cfg = cfg;
 
+function serializeQuery(obj, prefix) {
+  let str = [], p;
+  for(p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      let k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+      str.push((v !== null && typeof v === "object") ?
+        serialize(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+  }
+  return str.join("&");
+}
+module.exports.serializeQuery = serializeQuery;
 
-module.exports.getResource = (endpoint, query) => {
-  let apiEndpoint = `${cfg.api_prefix}/${endpoint}?${query}`;
+module.exports.getResource = (endpoint, params = {}) => {
+  let queryParams = serializeQuery(params);
+
+  if (queryParams) queryParams = `?${queryParams}`;
+
+  let apiEndpoint = `${cfg.api_prefix}/${endpoint}${queryParams}`;
 
   return new Promise((resolve, reject) => {
     request(apiEndpoint, (err, resp, body) => {
