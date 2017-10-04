c8accc4c9f37f8f4ffda29d54c8337c2157585d5
Remmy <goremmy@gmail.com>
2017-10-03 20:35:46 -0400

    add shared getResource method

--- src/js/core.js
+++ src/js/core.js
@@ -1,9 +1,23 @@
 
 var fs = require('fs');
 var path = require('path');
+var request = require('request');
 
 var allCfg = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 var nodeEnv = process.env.NODE_ENV || 'development';
 var cfg = allCfg[nodeEnv];
 
 module.exports.cfg = cfg;
+
+
+module.exports.getResource = (endpoint, query) => {
+  let apiEndpoint = `${cfg.api_prefix}/${endpoint}?${query}`;
+
+  return new Promise((resolve, reject) => {
+    request(apiEndpoint, (err, resp, body) => {
+      if(err) reject(err);
+
+      return resolve(resp.body);
+    });
+  });
+}
