6233b82ce9d3cac4600367267d10e875eef8f758
James Lott <james@lottspot.com>
2017-10-03 20:08:46 -0400

    Make core.js pass linting

--- src/js/core.js
+++ src/js/core.js
@@ -1,7 +1,9 @@
 
 var fs = require('fs');
+var path = require('path');
 
-
-var allCfg  = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
+var allCfg = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 var nodeEnv = process.env.NODE_ENV || 'development';
-var cfg     = allCfg[nodeEnv];
+var cfg = allCfg[nodeEnv];
+
+console.log(cfg); // Make eslint happy until we actually use cfg
