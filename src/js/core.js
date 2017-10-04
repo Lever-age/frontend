438311ea1d600a4958961f1591e0421a6c9dd09f
James Lott <james@lottspot.com>
2017-10-03 20:14:25 -0400

    Export cfg object from core

--- src/js/core.js
+++ src/js/core.js
@@ -6,4 +6,4 @@ var allCfg = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 var nodeEnv = process.env.NODE_ENV || 'development';
 var cfg = allCfg[nodeEnv];
 
-console.log(cfg); // Make eslint happy until we actually use cfg
+module.exports.cfg = cfg;
