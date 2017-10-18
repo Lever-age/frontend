33abf9ecfa133aa570e52f531711cd9388951b53
Remmy <goremmy@gmail.com>
2017-10-17 20:02:05 -0400

    Add contributions info from api

--- src/js/candidate-detail.js
+++ src/js/candidate-detail.js
@@ -1,18 +1,18 @@
-let htmlFileName = __filename.split('/').pop().replace('.js', '.html');
 
 module.exports = (core) => {
+  let htmlFileName = __filename.split('/').pop().replace('.js', '.html');
   let slug = core.getUrlParameter('slug') || window.location.pathname.split('/')[2];
   let pageCfg = core.cfg.pages[htmlFileName];
 
   core.getResource('candidates', { candidate_slug: slug }).then((candidate) => {
-    console.log(candidate.data[0]);
+    if (candidate.data[0]) {
+      console.log(candidate.data[0]);
 
-    candidate.data[0].name_full = `${candidate.data[0].name_first}  ${candidate.data[0].name_last}`;
-
-    // let output = core.Mustache.render(tpl, { candidate: candidate.data[0] });
-    // document.getElementById(pageCfg.mainContainer).innerHTML = output;
-    // document.title = candidate.data[0].name_full;
-    let nodes = core.renderTemplate(pageCfg.mainTemplateId, { candidate: candidate.data[0] });
-    core.fillContainer(pageCfg.mainContainer, nodes);
+      candidate.data[0].name_full = `${candidate.data[0].name_first}  ${candidate.data[0].name_last}`;
+      let nodes = core.renderTemplate(pageCfg.mainTemplateId, { candidate: candidate.data[0] });
+      core.fillContainer(pageCfg.mainContainer, nodes);
+    } else {
+      core.fillContainer(pageCfg.mainContainer, 'Candidate not found');
+    }
   }).catch((err) => core.handleError(err, pageCfg.mainContainer));
 };
