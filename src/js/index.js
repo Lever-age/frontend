66d89e1a2aace730d434f81960b0068f3a6dfe8a
James Lott <james@lottspot.com>
2017-10-17 21:53:34 -0400

    Display races on index page

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-module.exports = (core) => console.log(JSON.stringify(core.cfg));
+module.exports = (core) => {
+  let local = { cfg: core.cfg.pages[core.getPageName()] };
+  local.context = { race_detail_pathname: core.cfg.pathnames.raceDetail };
+
+  core.getResource(local.cfg.resource).then((races) => {
+    local.context.races = races.data || [];
+    local.cfg.renderMap.forEach((map) => {
+      let fragment = core.renderTemplate(map.template, local.context);
+      core.fillContainer(map.container, fragment);
+    });
+  });
+};
